6d7afd24b5362b6b2e7f5418041e2d05f19a3f01
Roger Farrell <127912262+RogerFarrell@users.noreply.github.com>
2024-03-27 18:16:55 -0500

    Quartz sync: Mar 27, 2024, 6:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Roger Farrell's blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
