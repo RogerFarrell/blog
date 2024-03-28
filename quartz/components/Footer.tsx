bec7be2a490a98a8662c71600d9ee841d587a8e4
Roger Farrell <127912262+RogerFarrell@users.noreply.github.com>
2024-03-27 21:23:31 -0500

    Quartz sync: Mar 27, 2024, 9:23 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,8 +13,9 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        {/* <hr />
-        <p>
+        <hr />
+        <p>© {year} Roger Farrell</p>
+        {/* <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
